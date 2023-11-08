ba24ffc7d6c6f5021bed4413f0767e9106edf92f
archeryoli <oliver.brandstetter2506@gmail.com>
2023-11-08 15:19:20 +0100

    idk

--- Poker/main.py
+++ Poker/main.py
@@ -1,6 +1,6 @@
 import random
-import numpy as np
 from matplotlib import pyplot as plt
+import unittest
 
 cards = list(range(0, 52))
 
@@ -89,7 +89,7 @@ def check_straight(cards):
   n = list(range(min_num, min_num+5))
   n.sort()
   numbers.sort()
-  royal_straight = [1, 10, 11, 12, 9]
+  royal_straight = [0, 10, 11, 12, 9]
   royal_straight.sort()
   return set(n) == set(numbers) or numbers == royal_straight
 
@@ -98,11 +98,11 @@ def draw(card_list, how_many):
   if check_flush(cards):
     values = list(map(lambda card: get_card(card)[1], cards))
     values.sort()
-    royal_straight = [1, 10, 11, 12, 9]
+    royal_straight = [0, 10, 11, 12, 9]
     royal_straight.sort()
 
     if values == royal_straight:
-      possibilities["royal_flush"]
+      possibilities["royal_flush"] += 1
     elif check_straight(cards):
       possibilities["straight_flush"] += 1
     else:
@@ -123,18 +123,50 @@ def draw(card_list, how_many):
     possibilities["high_card"] += 1
   
   
-  
-  
+class TestingPokerTests(unittest.TestCase):
+  def test_pair(self):
+    self.assertTrue(check_pair([0, 13, 14, 15, 16]))
+    self.assertFalse(check_pair([0, 12, 42, 14, 11]))
 
+  def test_two_pair(self):
+    self.assertTrue(check_two_pair([0, 13, 12, 25, 4]))
+
+  def test_three_of_a_kind(self):
+    self.assertTrue(check_three_of_a_kind([0, 13, 26, 27, 51]))
+
+  def test_four_of_a_kind(self):
+    self.assertTrue(check_four_of_a_kind([0, 13, 26, 39, 51]))
+  
+  def test_flush(self):
+    self.assertTrue(check_flush([0, 1, 2, 3, 4]))
+    self.assertFalse(check_flush([0, 1, 2, 3, 14]))
+
+  def test_full_house(self):
+    self.assertTrue(check_full_house([0, 13, 26, 1, 14]))
+    self.assertFalse(check_full_house([0, 13, 26, 1, 15]))
+
+  def test_straight(self):
+    self.assertTrue(check_straight([0, 1, 2, 3, 4]))
+    self.assertTrue(check_straight([0, 10, 11, 12, 9]))
+    self.assertFalse(check_straight([0, 1, 2, 3, 5]))
+
+  def check_royal_flusH(self):
+    self.assertTrue(check_straight([13, 23, 24, 25, 22]))
+
+
+# unittest.main()
 draws = 1000000
 
 for i in range(draws):
-  draw(cards, 5)
+ draw(cards, 5)
 
 fig, ax = plt.subplots()
-
+sum = 0
 for key, value in possibilities.items():
   p = ax.bar(key, 100*value / draws, label=(str(100*value / draws) + " %"))
   ax.bar_label(p, label_type="edge")
-  print(key, 100 * value / draws, "%")
+  print(key, 100 * value / draws, "%", "(", value, ")")
+  sum += value
+print(sum)
 plt.show()
+
